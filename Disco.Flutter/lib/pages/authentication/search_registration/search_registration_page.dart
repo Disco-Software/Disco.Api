4225fc7068e7103545c068cffc3422e42c0734f4
Stanislav <skorchevskyi@zeushq.com>
2022-02-10 16:43:55 +0200

    Update 10.02.2022

---
+++ Disco.Flutter/lib/pages/authentication/search_registration/search_registration_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchRegistrationPage extends StatefulWidget {
+  const SearchRegistrationPage({Key key}) : super(key: key);
+
+  @override
+  _SearchRegistrationPageState createState() => _SearchRegistrationPageState();
+}
+
+class _SearchRegistrationPageState extends State<SearchRegistrationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
