e0e4a6ee248db6e0f4c1069db59e21a25f7be3d2
Stanislav <skorchevskyi@zeushq.com>
2022-02-15 16:22:58 +0200

    Update 15.02.2022

---
+++ Disco.Flutter/lib/pages/authentication/registration/registration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Registration extends StatefulWidget {
+  const Registration({Key key}) : super(key: key);
+
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
