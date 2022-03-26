c55614b4870457cfe8d1ce8f9d3b2503606640db
Stanislav <skorchevskyi@zeushq.com>
2022-03-26 20:05:08 +0100

    update 26.03.2022 v3

---
+++ Disco.Flutter/lib/core/widgets/unicorn_image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UnicornImage extends StatelessWidget {
+  const UnicornImage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
