28521630eaae4f34235789f2b6361afe8f8f443f
stas korchevskyi <stas_1999_nr@ukr.net>
2022-07-23 13:22:29 +0300

    Update infrastructure of story service

--- Disco.ApiServices/Controllers/UserStoryController.cs
+++ Disco.ApiServices/Controllers/UserStoryController.cs
@@ -14,35 +14,46 @@ namespace Disco.ApiServices.Controllers
     [Authorize(AuthenticationSchemes = AuthScheme.UserToken)]
     public class UserStoryController : ControllerBase
     {
-        private readonly IStoryService storyService;
-
-        public UserStoryController(IStoryService _storyService)
+        private readonly IStoryService _storyService;
+        private readonly IUserService _userService;
+        public UserStoryController(
+            IStoryService storyService,
+            IUserService userService)
         {
-            storyService = _storyService;
+            this._storyService = storyService;
+            this._userService = userService;
         }
 
         [HttpPost("create")]
         public async Task<IActionResult> Create([FromForm] CreateStoryDto model)
         {
-            return await storyService.CreateStoryAsync(model);
+            var user = await _userService.GetUserAsync(HttpContext.User);
+
+           var story = await _storyService.CreateStoryAsync(user, model);
+
+            return Ok(story);
         }
 
         [HttpDelete("{id:int}")]
         public async Task Delete([FromRoute] int id)
         {
-            await storyService.DeleteStoryAsync(id);
+            await _storyService.DeleteStoryAsync(id);
         }
 
         [HttpGet("get/{id:int}")]
         public async Task<ActionResult<Story>> GetStory([FromRoute] int id)
         {
-            return await storyService.GetStoryAsync(id);
+            return await _storyService.GetStoryAsync(id);
         }
 
         [HttpGet("all")]
         public async Task<ActionResult<List<Story>>> GetStoriesAsync([FromQuery] GetAllStoriesDto model)
         {
-            return await storyService.GetAllStoryAsync(model);
+            var user = await _userService.GetUserAsync(HttpContext.User);
+
+            var stories = await _storyService.GetAllStoryAsync(user, model);
+
+            return stories;
         }
 
     }
