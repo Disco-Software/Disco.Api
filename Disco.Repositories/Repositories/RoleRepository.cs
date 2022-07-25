38ba2ee82ce9124b485d1861514510a2a0bde13d
stas korchevskyi <stas_1999_nr@ukr.net>
2022-07-25 18:44:27 +0300

    Update infrastructure of AdminRoleController

---
+++ Disco.Repositories/Repositories/RoleRepository.cs
@@ -0,0 +1,32 @@
+﻿using Disco.Domain.EF;
+using Disco.Domain.Interfaces;
+using Disco.Domain.Models;
+using Disco.Domain.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disco.Domain.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly ApiDbContext _ctx;
+
+        public RoleRepository(ApiDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<List<Role>> GetAll(int pageNumber, int pageSize)
+        {
+            return await _ctx.Roles
+                 .OrderBy(x => x.Name)
+                 .Skip((pageNumber - 1) * pageSize)
+                 .Take(pageSize)
+                 .ToListAsync();
+        }
+    }
+}
