62017349071fab00379a69d233f4b5b9ed553ad0
stas korchevskyi <stas_1999_nr@ukr.net>
2022-07-25 17:56:10 +0300

    Update infrastructure of AdminAuthenticationController

--- Disco.Business/Interfaces/IAdminAuthenticationService.cs
+++ Disco.Business/Interfaces/IAdminAuthenticationService.cs
@@ -1,4 +1,5 @@
 ﻿using Disco.Business.Dtos.Authentication;
+using Disco.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -6,9 +7,9 @@ namespace Disco.Business.Interfaces
 {
     public interface IAdminAuthenticationService
     {
-        public Task<IActionResult> LogIn(LoginDto model);
-        public Task<IActionResult> RefreshToken(RefreshTokenDto model);
-        public Task<IActionResult> ForgotPassword(Dtos.Authentication.ForgotPasswordDto model);
-        public Task<IActionResult> ResetPassword(Dtos.Authentication.ResetPasswordDto model);
+        public Task<UserResponseDto> LogIn(User user, LoginDto dto);
+        public Task<UserResponseDto> RefreshToken(RefreshTokenDto dto);
+        public Task<string> ForgotPassword(User user, ForgotPasswordDto dto);
+        public Task<UserResponseDto> ResetPassword(User user, ResetPasswordDto dto);
     }
 }
