35337378d1fc71a746b73897d88f8246844a7379
Vladyslav Kyrychenko <36587268+Alksgest@users.noreply.github.com>
2022-08-04 19:08:44 +0300

    Fixed story test

--- Disco.Tests/Services/StoryServiceTest.cs
+++ Disco.Tests/Services/StoryServiceTest.cs
@@ -23,6 +23,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Disco.Domain.Interfaces;
+using Profile = Disco.Domain.Models.Profile;
 
 namespace Disco.Tests.Services
 {
@@ -32,83 +34,78 @@ namespace Disco.Tests.Services
         [TestMethod]
         public async Task CreateStory_ReturnsSuccessResponse()
         {
-            try
+            const string content = "Hello World from a Fake File";
+            const string fileName = "test.pdf";
+            using var stream = new MemoryStream();
+            using var writer = new StreamWriter(stream);
+
+            await writer.WriteAsync(content);
+            await writer.FlushAsync();
+
+            stream.Position = 0;
+
+            IFormFile file = new FormFile(stream, 0, stream.Length, "id_from_form", fileName);
+
+            var user = new User
             {
-                var content = "Hello World from a Fake File";
-                var fileName = "test.pdf";
-                var stream = new MemoryStream();
-                var writer = new StreamWriter(stream);
-                writer.Write(content);
-                writer.Flush();
-                stream.Position = 0;
-
-                IFormFile file = new FormFile(stream, 0, stream.Length, "id_from_form", fileName);
-
-                var user = new User
+                UserName = "Vasya_Pupkin",
+                Email = "pupkin2022@gmail.com",
+                Profile = new Domain.Models.Profile
                 {
-                    UserName = "Vasya_Pupkin",
-                    Email = "pupkin2022@gmail.com",
-                    Profile = new Domain.Models.Profile
+                    Status = StatusProvider.MusicLover,
+                    Id = 5,
+                }
+            };
+
+            var story = new Story
+            {
+                Id = 1,
+                StoryImages = new List<StoryImage>(),
+                DateOfCreation = DateTime.UtcNow,
+                StoryVideos = new List<StoryVideo>(),
+                Profile = user.Profile,
+                ProfileId = user.Profile.Id
+            };
+
+            user.Profile.UserId = user.Id;
+            user.Profile.User = user;
+            
+            var mockedStoryRepository = new Mock<IStoryRepository>();
+
+            mockedStoryRepository
+                .Setup(s => s.AddAsync(It.IsAny<Story>(), It.IsAny<Profile>()))
+                .Returns(Task.CompletedTask);
+
+            var mockedStoryImageService = new Mock<IStoryImageService>();
+            mockedStoryImageService.Setup(s =>
+                    s.CreateStoryImageAsync(new CreateStoryImageDto
                     {
-                        Status = StatusProvider.MusicLover,
-                        Id = 5,
-                    }
-                };
-
-                var story = new Story
+                        StoryId = 2,
+                        StoryImageFile = file
+                    }))
+                .Returns(Task.FromResult(new StoryImage
                 {
-                    Id = 1,
-                    StoryImages = new List<StoryImage>(),
-                    DateOfCreation = DateTime.UtcNow,
-                    StoryVideos = new List<StoryVideo>(),
-                    Profile = user.Profile,
-                    ProfileId = user.Profile.Id
-                };
+                    Source = file.FileName,
+                    DateOfCreation = DateTime.UtcNow
+                }));
 
-                user.Profile.UserId = user.Id;
-                user.Profile.User = user;
-
-                var ctxOptions = new DbContextOptionsBuilder<ApiDbContext>()
-                    .UseInMemoryDatabase<ApiDbContext>("Disco.Database")
-                    .Options;
-
-                var mockedRepository = new Mock<StoryRepository>(new ApiDbContext(ctxOptions));
-
-                mockedRepository
-                    .Setup(s => s.AddAsync(story))
-                    .Returns(Task.CompletedTask);
-
-                var mockedStoryImage = new Mock<IStoryImageService>();
-                mockedStoryImage.Setup(s =>
-                    s.CreateStoryImageAsync(new CreateStoryImageDto { StoryId = 2, StoryImageFile = file }))
-                        .Returns(Task.FromResult(new StoryImage { Source = file.FileName, DateOfCreation = DateTime.UtcNow }));
-
-                var mockedStoryVideo = new Mock<IStoryVideoService>();
-
-                var storyDto = new CreateStoryDto
-                {
-                    ProfileId = user.Profile.Id,
-                    StoryImages = new List<IFormFile>(),
-                    StoryVideos = new List<IFormFile>(),
-                };
-
-                storyDto.StoryImages.Add(file);
-
-                var mapperConfiguration = new MapperConfiguration(config => config.AddProfile<MapProfile>());
-                var mapper = mapperConfiguration.CreateMapper();
-
-                var service = new StoryService(mockedRepository.Object, mockedStoryImage.Object, null, mapper);
-                var response = await service.CreateStoryAsync(user, storyDto);
-
-                Assert.IsNotNull(response.StoryImages);
-                Assert.IsNotNull(response.DateOfCreation);
-
-            }
-            catch (Exception ex)
+            var storyDto = new CreateStoryDto
             {
+                ProfileId = user.Profile.Id,
+                StoryImages = new List<IFormFile>(),
+                StoryVideos = new List<IFormFile>(),
+            };
 
-                throw;
-            }
+            storyDto.StoryImages.Add(file);
+
+            var mapperConfiguration = new MapperConfiguration(config => config.AddProfile<MapProfile>());
+            var mapper = mapperConfiguration.CreateMapper();
+
+            var service = new StoryService(mockedStoryRepository.Object, mockedStoryImageService.Object, null, mapper);
+            var response = await service.CreateStoryAsync(user, storyDto);
+
+            Assert.IsNotNull(response.StoryImages);
+            Assert.IsNotNull(response.DateOfCreation);
         }
     }
 }
